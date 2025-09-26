f96ed59249aaff9584ea2ada261316d8deaaf701
Student <Student@290-17>
2025-09-26 15:43:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,423 @@
+﻿using System.Xml.Linq;
+
+class InvalidNameException : Exception
+{
+    public InvalidNameException(string message) : base(message) { }
+}
+class InvalidInputException : Exception
+{
+    public InvalidInputException(string message) : base(message) { }
+}
+public enum Items
+{
+    Bible,
+    Tabaco,
+    Lamp,
+    Towel,
+    Apple,
+    Bottle,
+    Cigarette,
+    Fish,
+    Meat
+}
+
+interface ILightable
+{
+    void LightLamp();
+}
+interface IGotHereable
+{
+    void GotHere();
+}
+class Person : ILightable, IGotHereable
+{
+    private string Name;
+    public string name
+    {
+        get { return Name; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Имя не может быть пустым");
+            }
+            Name = value;
+        }
+    }
+    private string Spot;
+    public string spot
+    {
+        get { return Spot; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Место не может быть пустым");
+            }
+            Spot = value;
+        }
+    }
+    public bool IsSick { get; set; }
+    public bool IsTired { get; set; }
+    public bool IsHungry { get; set; }
+    public bool IsSickDealer { get; set; }
+    public bool IsHungryDealer { get; set; }
+    public bool IsTiredDealer { get; set; }
+    public bool IsMoney { get; set; }
+    public bool PassedByDealer { get; set; }
+    public bool MurderDealer { get; set; }
+    public Bag Inventory { get; set; } = new Bag();
+
+    public Person(string name, string spot)
+    {
+        Name = name;
+        Spot = spot;
+    }
+    public void LeaveHome()
+    {
+        int k = 0;
+        Console.WriteLine($"{Name} хочет выйти уже прохладным вечером на поиски еды");
+        Console.WriteLine("Надеть кожанную куртку или рубашку (1 or 2)?");
+        string input1 = Console.ReadLine();
+
+        if (input1 == "1")
+        {
+            IsSick = false;
+            IsMoney = true;
+        }
+        else if (input1 == "2")
+        {
+            IsSick = true;
+            IsMoney = false;
+        }
+        else
+        {
+            throw new InvalidInputException("Введите 1 или 2");
+        }
+
+        Console.WriteLine($"Надеть удобные ботинки или лапти (1 or 2)? ");
+        string input2 = Console.ReadLine();
+        if (input2 == "1")
+        {
+            IsTired = false;
+        }
+        else if (input2 == "2")
+        {
+            IsTired = true;
+            k++;
+            IsHungry = true;
+        }
+        else
+        {
+            throw new InvalidInputException("Введите 1 или 2");
+        }
+        Console.WriteLine($"\n{Name} отправился в {Spot} на поиске еды");
+        Console.WriteLine("На встречу шел торговец с припасами, он остановился и начал предлагать товары");
+        if (IsMoney)
+        {
+            IsHungry = false;
+            PassedByDealer = false;
+            IsSickDealer = false;
+            IsTiredDealer = false;
+            Console.WriteLine($"Из за того что {Name} надел кожаную куртку, в карманах оказались деньги, и он смог купить себе поесть");
+            Inventory.AddItems(Items.Apple); Inventory.AddItems(Items.Bottle);
+            Console.WriteLine($"{Name} положил купленные предметы в походную сумку");
+        }
+        else
+        {
+            Console.WriteLine("Денег не оказалось, но можно убить торговца и забрать еду, или пройти мимо на поиски (1 or 2)");
+            string input3 = Console.ReadLine();
+            if (input3 == "1")
+            {
+                MurderDealer = true;
+                IsTiredDealer = true;
+                IsHungry = false;
+                IsTired = true;
+                k++;
+                if (k == 2)
+                {
+                    IsSickDealer = true;
+                }
+                Console.WriteLine($"{Name} убил торговца, забрал еду, поэтому он точно не останется голодным");
+                Inventory.AddItems(Items.Meat); Inventory.AddItems(Items.Apple); Inventory.AddItems(Items.Cigarette);
+                Console.WriteLine($"{Name} положил награбленые предметы в походную сумку");
+            }
+            else if (input3 == "2")
+            {
+                IsSickDealer = false;
+                PassedByDealer = true;
+                IsTiredDealer = false;
+                Console.WriteLine($"Из за того что денег нет, {Name} решил пройти мимо дальше на поиски еды");
+                if (input2 == "1")
+                {
+                    Inventory.AddItems(Items.Fish); Inventory.AddItems(Items.Apple);
+                    Console.WriteLine($"{Name} поймал рыбу и собрал яблоки, положил это в походную сумку");
+                }
+            }
+            else
+            {
+                throw new InvalidInputException("Введите 1 или 2");
+            }
+        }
+    }
+    public void GotHere()
+    {
+        Console.WriteLine($"\n{Name}, спустя некоторое время добрался до своего убежища");
+    }
+    public void LightLamp()
+    {
+        Console.WriteLine($"{Name} включил светильник, потому что в убежище было темно");
+    }
+    public void CanSleep()
+    {
+        if (IsSick || IsSickDealer)
+        {
+            Console.WriteLine("Из за болезни сон никак не идет");
+        }
+        else if (!IsSick && !IsSickDealer)
+        {
+            Console.WriteLine($"{Name} спокойно уснул");
+        }
+    }
+    public void Condition()
+    {
+        if (IsSick)
+        {
+            if (IsSickDealer)
+            {
+                Console.WriteLine($"В драке с торговцем {Name} очень вымотался, поэтому заболел");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} болен, потому что надел рубашку и простудился");
+            }
+        }
+        else Console.WriteLine($"{Name} надел куртку, поэтому чувствует себя хорошо");
+        if (IsTired)
+        {
+            if (IsTiredDealer)
+            {
+                Console.WriteLine($"{Name} устал из за драки с торговцем");
+            }
+            else
+            {
+                if (IsMoney)
+                {
+                    Console.WriteLine($"{Name} просто купил еды у торговца и вернулся домой, но его ноги устали из за лаптей");
+                }
+                else
+                {
+                    Console.WriteLine($"Только ноги {Name} сильно утомились из за лаптей");
+                }
+            }
+        }
+        else
+        {
+            if (IsMoney)
+            {
+                Console.WriteLine($"{Name} купил товары и сразу же пошел домой, поэтому не сильно устал");
+            }
+            else if (PassedByDealer)
+            {
+                Console.WriteLine($"{Name} не потратил свои силы на убийство торговца");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} не чувствует усталости в ногах, потому что он надел удобные ботинки");
+            }
+        }
+        if (IsHungry)
+        {
+            Console.WriteLine($"{Name} ничего не купил у торговца и не нашел поесть, потому что он не смог дойти до места с едой");
+        }
+        else
+        {
+            if (IsMoney)
+            {
+                Console.WriteLine($"{Name} купил еды у торговца, поэтому сытый");
+            }
+            else if (MurderDealer)
+            {
+                Console.WriteLine($"{Name} убил торговца и забрал еду, поэтому сытый");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} дошел до {Spot} с едой, поэтому сытый");
+            }
+        }
+        CanSleep();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Person other)
+        {
+            return Name == other.Name;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}";
+    }
+}
+abstract class Storage
+{
+    public List<Items> items = new List<Items>();
+
+    public abstract void Info();
+    public virtual void PrintItems()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public virtual void AddItems(Items name)
+    {
+        if (items.Contains(name))
+        {
+            throw new InvalidNameException("Такой предмет уже существует");
+        }
+        else items.Add(name);
+    }
+
+}
+class Chest : Storage
+{
+    public override void Info()
+    {
+        Console.WriteLine("\nПосле непродолжительного сна герой пошел к сундуку за табаком и библией");
+    }
+    public override void PrintItems()
+    {
+        Console.WriteLine("Содержимое сундука:");
+        base.PrintItems();
+    }
+    public override void AddItems(Items name)
+    {
+        base.AddItems(name);
+    }
+    public void GetItem(Items name)
+    {
+        if (!items.Contains(name))
+        {
+            Console.WriteLine($"Предмета {name} нет в сундуке");
+            return;
+        }
+        foreach (var item in items)
+        {
+            if (item.Equals(name))
+            {
+                Console.WriteLine($"Вы достали {name} из сундука");
+                items.Remove(name);
+                break;
+            }
+        }
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is Chest other)
+        {
+            return items == other.items;
+        }
+
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(items);
+    }
+    public override string ToString()
+    {
+        return $"{items}";
+    }
+
+}
+class Bag : Storage
+{
+    public override void Info()
+    {
+        Console.WriteLine("\nГлавный герой откыл сумку");
+    }
+    public override void PrintItems()
+    {
+        Console.WriteLine("Содержмое сумки: ");
+        base.PrintItems();
+    }
+    public override void AddItems(Items name)
+    {
+        if (items.Count >= 4)
+        {
+            Console.WriteLine("Место в сумке закончилось");
+            return;
+        }
+        base.AddItems(name);
+    }
+    public void GetItem(Items name)
+    {
+        if (!items.Contains(name))
+        {
+            Console.WriteLine($"Предмета {name} нет в сумке");
+            return;
+        }
+        foreach (var item in items)
+        {
+            if (item.Equals(name))
+            {
+                Console.WriteLine($"Вы достали {name} из сумки");
+                items.Remove(name);
+                break;
+            }
+        }
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is Bag other)
+        {
+            return items == other.items;
+        }
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(items);
+    }
+    public override string ToString()
+    {
+        return $"{items}";
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Робин", "Долина");
+        person.LeaveHome();
+        person.GotHere();
+        person.LightLamp();
+        person.Inventory.Info();
+        person.Inventory.PrintItems();
+        person.Inventory.GetItem(Items.Apple);
+        Console.WriteLine("Герой поел яблоко");
+        person.Condition();
+
+        Chest chest = new Chest();
+        chest.Info();
+        chest.AddItems(Items.Tabaco);
+        chest.AddItems(Items.Bible);
+        chest.AddItems(Items.Cigarette);
+        chest.AddItems(Items.Lamp);
+        chest.AddItems(Items.Towel);
+        chest.PrintItems();
+        chest.GetItem(Items.Bible);
+        chest.GetItem(Items.Tabaco);
+    }
+}
