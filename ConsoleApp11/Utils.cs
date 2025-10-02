48a32314a3913c305632c470fc4062ff65045576
Иван <ivan.i.melnik007@gmail.com>
2025-10-02 14:54:08 +0300

    some refactoring

---
+++ ConsoleApp11/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp11;
+
+namespace ConsoleApp11
+{
+    class InvalidNameException : Exception
+    {
+        public InvalidNameException(string message) : base(message) { }
+    }
+    class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message) { }
+    }
+    public enum Items
+    {
+        Bible,
+        Tabaco,
+        Lamp,
+        Towel,
+        Apple,
+        Bottle,
+        Cigarette,
+        Fish,
+        Meat
+    }
+
+    interface ILightable
+    {
+        void LightLamp();
+    }
+    interface IGotHereable
+    {
+        void GotHere();
+    }
+}
